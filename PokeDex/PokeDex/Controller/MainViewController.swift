47c1e50e33fb259b2bdbf002610822a0cff53d19
Jesus <j.rendons@yahoo.com>
2023-04-30 20:25:58 -0600

    First commit, creation of all the folders

---
+++ PokeDex/PokeDex/Controller/MainViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  PokeDex
+//
+//  Created by Jesus Rendon on 4/30/23.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
